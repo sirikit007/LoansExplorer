3d0e3630e3fff9520ef90f59fc4343a1e29b17dc
sirikit007 <59901896+sirikit007@users.noreply.github.com>
2024-01-09 11:57:30 +1100

    1

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 //add hover effect yo side-menu list
+let navbar = document.querySelector('#navContent');
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset > 600) {
+        navbar.classList.add('bg-dark', 'shadow');
+    } else {
+        navbar.classList.remove('bg-dark', 'shadow');
+    }
+});
+
+//rotate angle
+let rotateAngle = document.querySelector('.rotateAngle');
+let collapseSidebar = document.getElementById('collapseSidebar');
+collapseSidebar.addEventListener('click', function() {
+    rotateAngle.classList.toggle('rotate');
+}
+)
+
+
+
+
 let list = document.querySelectorAll('.menuNavigation li');
 function activeLink() {
     list.forEach(item=> {
